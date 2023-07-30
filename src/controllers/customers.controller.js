0da718930b6b08bfba38a56147fd5002d4a7d17a
ricarvsan <ricarvsan@gmail.com>
2023-07-30 05:17:20 -0300

    ajustes customers date

--- src/controllers/customers.controller.js
+++ src/controllers/customers.controller.js
@@ -3,6 +3,7 @@ import { db } from "../database/database.connection.js";
 export async function getCustomers(req, res) {
     try {
         const customers = await db.query(`SELECT * FROM customers;`);
+        customers.rows.map(obj => obj.birthday = obj.birthday.toISOString().substring(0, 10));
         res.send(customers.rows);
     } catch (err) {
         res.status(500).send(err.message);
@@ -15,7 +16,7 @@ export async function getCustomersById(req, res) {
     try {
         const customer = await db.query(`SELECT * FROM customers WHERE id = $1;`, [id]);
         if(!customer.rows[0]) return res.sendStatus(404);
-        console.log(customer.rows[0]);
+        customer.rows[0].birthday = customer.rows[0].birthday.toISOString().substring(0, 10);
         res.send(customer.rows[0]);
     } catch (err) {
         res.status(500).send(err.message);
@@ -24,6 +25,8 @@ export async function getCustomersById(req, res) {
 
 export async function insertCustomer(req, res) {
     const {name, phone, cpf, birthday} = req.body;
+
+    if(!name) return res.sendStatus(400);
 
     try {
         const cpfInUse = await db.query(`SELECT * FROM customers WHERE cpf = $1;`, [cpf]);
@@ -45,11 +48,9 @@ export async function updateCustomer(req, res) {
     const {name, phone, cpf, birthday} = req.body;
 
     try {
-        const cpfInUse = await db.query(`SELECT * FROM customers WHERE cpf = $1 AND id <> $2;`, [cpf, id]);
+        const cpfInUse = await db.query(`SELECT * FROM customers WHERE cpf = $1 AND id = $2;`, [cpf, id]);
         if(cpfInUse.rows[0]) return res.sendStatus(404);
-        await db.query(`UPDATE customers SET name = $1 phone = $2 cpf = $3 birthday = $4 WHERE id = $5;`, 
-        [name, phone, cpf, birthday, id]
-        );
+        
         res.sendStatus(200);
     } catch (err) {
         res.status(500).send(err.message);
--- src/schemas/customers.schemas.js
+++ src/schemas/customers.schemas.js
@@ -1,7 +1,7 @@
 import Joi from "joi"
 
 export const customerSchema = Joi.object({
-    name: Joi.string().min(3).required(),
+    name: Joi.string(),
     phone: Joi.string().pattern(new RegExp('^[0-9]{10,11}$')),
     cpf: Joi.string().pattern(new RegExp('^[0-9]{11}$')),
     birthday: Joi.date().iso().required()
